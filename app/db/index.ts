1951c83849a52fc3b0f0c3b5b34951ba09bdb79f
Kevin Peredo <46333814+kveperedo@users.noreply.github.com>
2024-03-30 20:53:16 +0800

    Revert "Fix db error in prod"

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,8 +1,9 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import envServerSchema from '~/env.server';
+import * as schema from './schema';
 
 const sql = neon(envServerSchema.DATABASE_URL);
-const db = drizzle(sql);
+const db = drizzle(sql, { schema });
 
 export default db;
