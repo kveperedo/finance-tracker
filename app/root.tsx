aabd5059e974261612f538d5846f9e371a759718
Kevin Peredo <kveperedo@gmail.com>
2024-04-01 17:54:39 +0800

    Add global progress bar

--- app/root.tsx
+++ app/root.tsx
@@ -1,34 +1,25 @@
-import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-} from '@remix-run/react';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 import { Analytics } from '@vercel/analytics/react';
 import type { LinksFunction } from '@vercel/remix';
 import stylesheet from '~/tailwind.css?url';
 
 import '@fontsource/zilla-slab';
 import '@fontsource-variable/source-code-pro';
+import ProgressBar from './components/progress-bar';
 
-export const links: LinksFunction = () => [
-    { rel: 'stylesheet', href: stylesheet },
-];
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }];
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html className="font-serif" lang="en">
             <head>
                 <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <Meta />
                 <Links />
             </head>
             <body className="flex h-screen flex-col overflow-hidden">
+                <ProgressBar />
                 {children}
                 <ScrollRestoration />
                 <Scripts />
