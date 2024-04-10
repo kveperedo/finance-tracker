328e568bf1d3c558ba1ada8577c74f5e2722aa63
Kevin Peredo <kveperedo@gmail.com>
2024-04-10 15:48:31 +0800

    Add expenses empty state

--- app/routes/_app.expenses/route.tsx
+++ app/routes/_app.expenses/route.tsx
@@ -110,6 +110,7 @@ export default function ExpensesPage() {
               ...expenses,
           ]
         : expenses;
+    const hasEmptyExpenses = expensesToDisplay.length === 0;
 
     return (
         <main className="container m-auto mx-auto flex w-full flex-1 flex-col overflow-hidden">
@@ -118,41 +119,53 @@ export default function ExpensesPage() {
                 <AddExpenseModal formMethods={formMethods} />
             </div>
 
-            <div className="flex justify-between rounded bg-stone-100 p-2">
-                <span className="flex-1 text-xs font-bold text-stone-500">Description</span>
-                <span className="basis-[100px] text-xs font-bold text-stone-500">Date</span>
-                <span className="basis-[100px] text-right text-xs font-bold text-stone-500">Amount</span>
-            </div>
+            {hasEmptyExpenses ? (
+                <div className="flex flex-1 flex-col items-center justify-center gap-1">
+                    <p className="font-semibold">No expenses this month!</p>
+                    <p className="text-sm text-stone-500">Start adding expenses by clicking the button above.</p>
+                </div>
+            ) : (
+                <>
+                    <div className="flex justify-between rounded bg-stone-100 p-2">
+                        <span className="flex-1 text-xs font-bold text-stone-500">Description</span>
+                        <span className="basis-[100px] text-xs font-bold text-stone-500">Date</span>
+                        <span className="basis-[100px] text-right text-xs font-bold text-stone-500">Amount</span>
+                    </div>
 
-            <div ref={tableRef} className="flex-1 overflow-scroll">
-                {expensesToDisplay.map((expense) => {
-                    const isOptimistic = pendingExpense?.id === expense.id;
+                    <div ref={tableRef} className="flex-1 overflow-scroll">
+                        {expensesToDisplay.map((expense) => {
+                            const isOptimistic = pendingExpense?.id === expense.id;
 
-                    return (
-                        <div
-                            className={cn(
-                                'flex items-center justify-between border-b border-b-stone-100 p-2 text-sm text-stone-600 transition-colors',
-                                isOptimistic && 'bg-lime-200'
-                            )}
-                            key={expense.id}>
-                            <span className="flex-1">{expense.description}</span>
-                            <span className="basis-[100px] font-mono text-sm">
-                                {format(expense.createdAt, 'MM-dd-yyyy')}
-                            </span>
-                            <span className="basis-[100px] text-right font-mono text-sm">
-                                {numberFormatter.format(parseFloat(expense.amount))}
-                            </span>
-                        </div>
-                    );
-                })}
-            </div>
+                            return (
+                                <div
+                                    className={cn(
+                                        'flex items-center justify-between border-b border-b-stone-100 p-2 text-sm text-stone-600 transition-colors',
+                                        isOptimistic && 'bg-lime-200'
+                                    )}
+                                    key={expense.id}
+                                >
+                                    <span className="flex-1">{expense.description}</span>
+                                    <span className="basis-[100px] font-mono text-sm">
+                                        {format(expense.createdAt, 'MM-dd-yyyy')}
+                                    </span>
+                                    <span className="basis-[100px] text-right font-mono text-sm">
+                                        {numberFormatter.format(parseFloat(expense.amount))}
+                                    </span>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </>
+            )}
 
-            <div className="flex items-center justify-end p-4">
-                <p className="font-mono text-xl font-bold">
-                    <span className="font-serif text-sm font-extralight">PHP </span>
-                    {numberFormatter.format(totalMonthlyExpenses)}
-                </p>
-            </div>
+            {!hasEmptyExpenses && (
+                <div className="flex items-center justify-end p-4">
+                    <p className="font-mono text-xl font-bold">
+                        <span className="font-serif text-sm font-extralight">PHP </span>
+                        {numberFormatter.format(totalMonthlyExpenses)}
+                    </p>
+                </div>
+            )}
         </main>
     );
 }
