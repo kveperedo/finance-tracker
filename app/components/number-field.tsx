de42d0a346ac163b6dd098704aead10466abe862
Kevin Peredo <kveperedo@gmail.com>
2024-04-17 08:23:05 +0800

    Add select all to number field on focus

--- app/components/number-field.tsx
+++ app/components/number-field.tsx
@@ -15,9 +15,16 @@ type NumberFieldProps = AriaNumberFieldProps & {
 export const NumberField = forwardRef<HTMLInputElement, NumberFieldProps>(
     ({ label, description, errorMessage, placeholder, ...props }, ref) => {
         return (
-            <AriaNumberField {...props} className={cn('flex flex-col items-start', props.className)}>
+            <AriaNumberField
+                {...props}
+                onFocus={(event) => {
+                    (event.target as HTMLInputElement).select();
+                    props.onFocus?.(event);
+                }}
+                className={cn('flex flex-col items-start', props.className)}
+            >
                 {label && <Label>{label}</Label>}
-                <Input ref={ref} className="font-mono text-sm" placeholder={placeholder} />
+                <Input ref={ref} className="text-sm" placeholder={placeholder} />
                 {description && <Text slot="description">{description}</Text>}
                 <FieldError className="mt-1 text-xs text-red-600">{errorMessage}</FieldError>
             </AriaNumberField>
